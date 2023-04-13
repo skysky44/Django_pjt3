acaa146c86dc58e3fa658945ec50070957de04bf
jeonghwan <ajh5251@gmail.com>
2023-04-13 16:08:03 +0900

    Update

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Review, Comment
 # Create your views here.
+def index(request):
+    reviews = Review.objects.all()
+
+
+    context = {
+        'reviews' : reviews,
+    }
+    return render(request, 'reviews/index.html', context)
