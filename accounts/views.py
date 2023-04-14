ce07a99ef711291eab14509195f6f163f9ba4572
YongJuDo <qud6330@naver.com>
2023-04-14 13:04:58 +0900

    Update 04

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,16 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from reviews.models import Review, Comment
 
 
 
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('reviews:index')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
 
@@ -29,8 +33,10 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
-
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('reviews:index')
+    
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -44,17 +50,18 @@ def login(request):
     return render(request, 'accounts/login.html', context)
 
 
-
-
+@login_required
 def logout(request):
     auth_logout(request)
     return redirect('reviews:index')
 
 
-
+@login_required
 def profile(request, user_pk):
     user = get_user_model().objects.get(pk=user_pk)
+    reviews = Review.objects.filter(user_id=user_pk)
     context = {
         'user' : user,
+        'reviews': reviews,
     }
     return render(request, 'accounts/profile.html', context)
--- reviews/views.py
+++ reviews/views.py
@@ -5,7 +5,6 @@ from .forms import ReviewForm, CommentForm
 def index(request):
     reviews = Review.objects.all()
 
-
     context = {
         'reviews' : reviews,
     }
@@ -15,7 +14,7 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save(commit=False)
             review.user = request.user
@@ -40,7 +39,6 @@ def detail(request, review_pk):
         'comment' : comment,
     }
     return render(request, 'reviews/detail.html', context)
-
 
 
 
@@ -70,3 +68,27 @@ def delete(request, review_pk):
     if request.user == review.user:
         review.delete()
     return redirect('reviews:index')
+
+
+def comment_create(request, review_pk):
+    if request.method == "POST":
+        review = Review.objects.get(pk=review_pk)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid:
+            comment = comment_form.save(commit=False)
+            comment.review = review
+            comment.user = request.user
+            comment.save()
+        return redirect('reviews:detail', review_pk)
+    context = {
+        'review': review,
+        'comment_form': comment_form,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+
+def comment_delete(request, review_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user:
+        comment.delete()
+    return redirect('reviews:detail', review_pk)
